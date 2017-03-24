f80caee45bcd0931057bd127937dcfef299132db
Brandon Kohn <blkohn@hotmail.com>
2017-03-24 09:59:58 -0400

    Updated scripts.

--- update.sh
+++ update.sh
@@ -5,39 +5,38 @@
 # Requires git and cmake on the path.
 # Note: Also requires the environment is configured for the specified CMAKE generator.
 
+function error_exit
+{
+    echo "$1" 1>&2
+    exit 1
+}
+
 if [ "$#" -ne 2 ]
   then
-    echo "Usage: ./update.sh <Install Path Prefix> <\"CMake Generator\">"
-    exit 1
+    error_exit "Usage: ./update.sh <Install Path Prefix> <\"CMake Generator\">"
 fi
 
 # Update the repo
 REPOSRC=https://github.com/brandon-kohn/simulation_suite.git
 
-git pull "$REPOSRC"
-ret=$?; if [[ $ret != 0 ]]; then exit $ret; fi
+git pull "$REPOSRC" || error_exit "Git pull failed. Aborting."
 
-git submodule update --init --recursive
-ret=$?; if [[ $ret != 0 ]]; then exit $ret; fi
+git submodule update --init --recursive || error_exit "Git submodule update failed. Aborting."
 
 INSTALL_PATH=$1
 CMAKE_GENERATOR=$2
 
 # Build the release
-cmake -H. -Bcmake.build -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX="$INSTALL_PATH" -G"$CMAKE_GENERATOR"
-ret=$?; if [[ $ret != 0 ]]; then exit $ret; fi
+cmake -H. -Bcmake.build -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX="$INSTALL_PATH" -G"$CMAKE_GENERATOR" || error_exit "CMake Release Build failed. Aborting."
 
 # Install it
-cmake --build "cmake.build" --target install
-ret=$?; if [[ $ret != 0 ]]; then exit $ret; fi
+cmake --build "cmake.build" --target install || error_exit "CMake Install failled. Aborting"
 
 # Build the debug
-cmake -H. -Bcmake.build -DCMAKE_BUILD_TYPE=DEBUG -DCMAKE_INSTALL_PREFIX="$INSTALL_PATH" -G"$CMAKE_GENERATOR"
-ret=$?; if [[ $ret != 0 ]]; then exit $ret; fi
+cmake -H. -Bcmake.build -DCMAKE_BUILD_TYPE=DEBUG -DCMAKE_INSTALL_PREFIX="$INSTALL_PATH" -G"$CMAKE_GENERATOR" || error_exit "CMake Debug Build failled. Aborting"
 
 # Install it
-cmake --build "cmake.build" --target install
-ret=$?; if [[ $ret != 0 ]]; then exit $ret; fi
+cmake --build "cmake.build" --target install || error_exit "CMake Install failled. Aborting"
 
 exit 0
 # End
