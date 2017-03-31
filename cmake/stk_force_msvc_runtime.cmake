5dc8917a1cc2c9eb4939f9b08bcb8ee170d3259f
Brandon Kohn <blkohn@hotmail.com>
2017-03-31 17:54:13 -0400

    Added cmake macro.

---
+++ cmake/stk_force_msvc_runtime.cmake
@@ -0,0 +1,22 @@
+macro(force_msvc_runtime)
+  if(MSVC)
+    if(NOT "${FORCE_MSVC_RUNTIME}" STREQUAL "")
+      # Set compiler options.
+      set(variables
+      CMAKE_C_FLAGS_DEBUG
+      CMAKE_C_FLAGS_MINSIZEREL
+      CMAKE_C_FLAGS_RELEASE
+      CMAKE_C_FLAGS_RELWITHDEBINFO
+      CMAKE_CXX_FLAGS_DEBUG
+      CMAKE_CXX_FLAGS_MINSIZEREL
+      CMAKE_CXX_FLAGS_RELEASE
+      CMAKE_CXX_FLAGS_RELWITHDEBINFO)      
+      message(STATUS "MSVC: Forcing runtime to ${FORCE_MSVC_RUNTIME}.")
+      foreach(variable ${variables})
+        if(${variable} matches "^/M[TD]d?$" regex)
+            string(regex replace "^/M[TD]d?$" "${FORCE_MSVC_RUNTIME}" ${variable} "${${variable}}")
+        endif()
+      endforeach()
+    endif()
+  endif()
+endmacro()
