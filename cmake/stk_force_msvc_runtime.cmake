0e47c481e3fc92449ec8ba3eff744f6a3ad6fea0
Brandon Kohn <blkohn@hotmail.com>
2017-03-31 18:59:25 -0400

    Added debug messages for cmake.

--- cmake/stk_force_msvc_runtime.cmake
+++ cmake/stk_force_msvc_runtime.cmake
@@ -1,16 +1,17 @@
 macro(force_msvc_runtime)
+  message(STATUS "force_msvc_runtime_called")
   if(MSVC)
     if(NOT "${FORCE_MSVC_RUNTIME}" STREQUAL "")
       # Set compiler options.
       set(variables
-      CMAKE_C_FLAGS_DEBUG
-      CMAKE_C_FLAGS_MINSIZEREL
-      CMAKE_C_FLAGS_RELEASE
-      CMAKE_C_FLAGS_RELWITHDEBINFO
-      CMAKE_CXX_FLAGS_DEBUG
-      CMAKE_CXX_FLAGS_MINSIZEREL
-      CMAKE_CXX_FLAGS_RELEASE
-      CMAKE_CXX_FLAGS_RELWITHDEBINFO)      
+          CMAKE_C_FLAGS_DEBUG
+          CMAKE_C_FLAGS_MINSIZEREL
+          CMAKE_C_FLAGS_RELEASE
+          CMAKE_C_FLAGS_RELWITHDEBINFO
+          CMAKE_CXX_FLAGS_DEBUG
+          CMAKE_CXX_FLAGS_MINSIZEREL
+          CMAKE_CXX_FLAGS_RELEASE
+          CMAKE_CXX_FLAGS_RELWITHDEBINFO)
       message(STATUS "MSVC: Forcing runtime to ${FORCE_MSVC_RUNTIME}.")
       foreach(variable ${variables})
         if(${variable} matches "^/M[TD]d?$" regex)
