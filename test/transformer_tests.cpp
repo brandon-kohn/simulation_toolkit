5dd312a65a689892cb91bfcc007b16153477979d
Brandon Kohn <blkohn@hotmail.com>
2017-08-24 17:46:32 -0400

    Updated test/transformer_tests.cpp to have pre and post multiplication policies as examples.

--- test/transformer_tests.cpp
+++ test/transformer_tests.cpp
@@ -229,6 +229,7 @@ TEST(TransformerTestSuite, test3DTransform)
 	auto v = vector3{ -19.286012 * units::si::meters, -38.724455 * units::si::meters, -3.590890 * units::si::meters };
 
 	using xform_t = transformer<3, premultiplication_policy>;
+	//using xform_t = transformer<3, premultiplication_policy>;
 	//transformer3 xform = transformer3().translate(make_point3(originA)).translate(v).rotate_x(roll).rotate_y(pitch).rotate_z(yaw).translate(-as_vector(make_point3(originB)));
 	xform_t xform = xform_t()
 		.translate(-as_vector(make_point3(originB)))
