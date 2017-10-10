a34d4d36311058b092c9bf34c34788bae856ba2e
Brandon Kohn <blkohn@hotmail.com>
2017-10-10 16:25:40 -0400

    Reverted change to remove bsptree and mesh from kernel.

--- stk/stk/geometry/geometry_kernel.hpp
+++ stk/stk/geometry/geometry_kernel.hpp
@@ -30,6 +30,9 @@
 #include "tensor/vector.hpp"
 #include "tensor/matrix.hpp"
 
+#include "space_partition/bsp_tree.hpp"
+#include "space_partition/mesh.hpp"
+
 #include "tolerance_policy.hpp"
 
 //! Load the expression template headers and common algorithms
