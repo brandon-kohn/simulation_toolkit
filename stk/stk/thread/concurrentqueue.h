0371f0dd12b6815fa42c1494f88c941fc41d9a89
Brandon Kohn <blkohn@hotmail.com>
2018-02-16 16:54:57 -0500

    Added moodycamel_concurrent_queue_traits_no_tokens.

--- stk/stk/thread/concurrentqueue.h
+++ stk/stk/thread/concurrentqueue.h
@@ -3748,6 +3748,55 @@ struct moodycamel_concurrent_queue_traits
 	}
 };
 
+struct moodycamel_concurrent_queue_traits_no_tokens
+{
+	template <typename T, typename Alloc = std::allocator<T>, typename Mutex = std::mutex>
+	using queue_type = moodycamel::ConcurrentQueue<T>;
+	using queue_info = void*;
+
+	template <typename T>
+	static queue_info get_queue_info(queue_type<T>&q)
+	{
+		return nullptr;
+	}
+
+	template <typename T, typename Value>
+	static bool try_push(queue_type<T>& q, queue_info queueInfo, Value&& value)
+	{
+		return q.enqueue(std::forward<Value>(value));
+	}
+
+	template <typename T>
+	static bool try_pop(queue_type<T>& q, queue_info queueInfo, T& value)
+	{
+		return q.try_dequeue(value);
+	}
+
+	template <typename T>
+	static bool try_steal(queue_type<T>& q, queue_info queueInfo, T& value)
+	{
+		return q.try_dequeue(value);
+	}
+
+	template <typename T, typename Value>
+	static bool try_push(queue_type<T>& q, Value&& value)
+	{
+		return q.enqueue(std::forward<Value>(value));
+	}
+
+	template <typename T>
+	static bool try_pop(queue_type<T>& q, T& value)
+	{
+		return q.try_dequeue(value);
+	}
+
+	template <typename T>
+	static bool try_steal(queue_type<T>& q, T& value)
+	{
+		return q.try_dequeue(value);
+	}
+};
+
 #if defined(__GNUC__)
 #pragma GCC diagnostic pop
 #endif
