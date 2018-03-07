242d87f60149776c3ae9bce303cd08bb2f099825
Brandon Kohn <blkohn@hotmail.com>
2018-03-07 14:50:51 -0500

    Added comment about requirement for quiesce call due to use of skipmap.

--- stk/stk/thread/thread_specific.hpp
+++ stk/stk/thread/thread_specific.hpp
@@ -46,169 +46,170 @@ namespace stk { namespace thread {
 
     }//! namespace detail;
 
-template <typename T>
-class thread_specific
-{
-    using data_ptr = T*;
-    using const_data_ptr = typename std::add_const<data_ptr>::type;
-    struct instance_map : std::map<thread_specific<T> const*, T>
+    //! Creates a thread local object which can be scoped to an objects lifetime. Must call quiesce occasionally to reclaim memory from threads going out of scope.
+    template <typename T>
+    class thread_specific
     {
-        instance_map() = default;
-        instance_map(const instance_map&)=delete;
-        instance_map& operator=(const instance_map&)=delete;
-        instance_map(instance_map&&)=delete;
-        instance_map& operator=(instance_map&&)=delete;
-        ~instance_map()
+        using data_ptr = T*;
+        using const_data_ptr = typename std::add_const<data_ptr>::type;
+        struct instance_map : std::map<thread_specific<T> const*, T>
         {
-            for (auto& i : *this)
+            instance_map() = default;
+            instance_map(const instance_map&)=delete;
+            instance_map& operator=(const instance_map&)=delete;
+            instance_map(instance_map&&)=delete;
+            instance_map& operator=(instance_map&&)=delete;
+            ~instance_map()
             {
-                if (i.first->m_deinitializer)
-                    i.first->m_deinitializer(i.second);
-                i.first->m_maps.erase(this);
+                for (auto& i : *this)
+                {
+                    if (i.first->m_deinitializer)
+                        i.first->m_deinitializer(i.second);
+                    i.first->m_maps.erase(this);
+                }
             }
-        }
-    };
-    friend struct instance_map;
-    static instance_map& hive();
+        };
+        friend struct instance_map;
+        static instance_map& hive();
 
-public:
+    public:
 
-    using reference = typename std::add_lvalue_reference<T>::type;
-    using const_reference = typename std::add_const<reference>::type;
+        using reference = typename std::add_lvalue_reference<T>::type;
+        using const_reference = typename std::add_const<reference>::type;
 
-    thread_specific()
-        : m_initializer([](){ return T();})
-    {}
+        thread_specific()
+            : m_initializer([](){ return T();})
+        {}
 
-    ~thread_specific()
-    {
-        destroy();
-    }
-
-    thread_specific(const thread_specific&) = delete;
-    thread_specific& operator=(const thread_specific&) = delete;
-    thread_specific(thread_specific&&) = delete;
-    thread_specific& operator=(thread_specific&&) = delete;
-
-    template <typename Initializer>
-    thread_specific( Initializer&& init )
-        : m_initializer( std::forward<Initializer>(init) )
-    {}
-
-    template <typename Initializer, typename Deinitializer>
-    thread_specific( Initializer&& init, Deinitializer&& deinit)
-        : m_initializer( std::forward<Initializer>(init) )
-        , m_deinitializer( std::forward<Deinitializer>(deinit) )
-    {}
-
-    template <typename Value>
-    thread_specific& operator = ( Value&& t )
-    {
-        auto pData = get_or_add_item();
-        GEOMETRIX_ASSERT(pData);
-        *pData = std::forward<Value>(t);
-        return *this;
-    }
-
-    reference operator *()
-    {
-        return *get_or_add_item();
-    }
-
-    const_reference operator *() const
-    {
-        return *get_or_add_item();
-    }
-
-    T* operator ->()
-    {
-        return get_or_add_item();
-    }
-
-    T const* operator ->() const
-    {
-        return get_or_add_item();
-    }
-
-    bool has_value_on_calling_thread() const
-    {
-        return get_item() != nullptr;
-    }
-
-    explicit operator bool()
-    {
-        return has_value_on_calling_thread();
-    }
-
-	void quiesce()
-	{
-		m_maps.quiesce();
-	}
-
-private:
-
-    data_ptr get_item() const
-    {
-        auto& m = hive();
-        auto iter = m.lower_bound(this);
-        if (iter != m.end() && iter->first == this)
-            return &iter->second;
-
-        return nullptr;
-    }
-
-    data_ptr get_or_add_item () const
-    {
-        auto& m = hive();
-        auto iter = m.lower_bound(this);
-        if(iter != m.end() && iter->first == this)
-            return &iter->second;
-
-        m_maps.insert(&m);
-        iter = m.insert(iter, std::make_pair(this, m_initializer()));
-        GEOMETRIX_ASSERT(iter != m.end());
-        return &iter->second;
-    }
-
-    void destroy()
-    {
-        auto& m = hive();
-        auto key = this;
-        if (!m_deinitializer)
+        ~thread_specific()
         {
-            m.erase(key);
-            std::for_each(m_maps.begin(), m_maps.end(), [key, this](instance_map* pMap)
-            {
-                pMap->erase(key);
-            });
+            destroy();
         }
-        else
+
+        thread_specific(const thread_specific&) = delete;
+        thread_specific& operator=(const thread_specific&) = delete;
+        thread_specific(thread_specific&&) = delete;
+        thread_specific& operator=(thread_specific&&) = delete;
+
+        template <typename Initializer>
+        thread_specific( Initializer&& init )
+            : m_initializer( std::forward<Initializer>(init) )
+        {}
+
+        template <typename Initializer, typename Deinitializer>
+        thread_specific( Initializer&& init, Deinitializer&& deinit)
+            : m_initializer( std::forward<Initializer>(init) )
+            , m_deinitializer( std::forward<Deinitializer>(deinit) )
+        {}
+
+        template <typename Value>
+        thread_specific& operator = ( Value&& t )
         {
-            auto it = m.find(key);
-            if (it != m.end())
+            auto pData = get_or_add_item();
+            GEOMETRIX_ASSERT(pData);
+            *pData = std::forward<Value>(t);
+            return *this;
+        }
+
+        reference operator *()
+        {
+            return *get_or_add_item();
+        }
+
+        const_reference operator *() const
+        {
+            return *get_or_add_item();
+        }
+
+        T* operator ->()
+        {
+            return get_or_add_item();
+        }
+
+        T const* operator ->() const
+        {
+            return get_or_add_item();
+        }
+
+        bool has_value_on_calling_thread() const
+        {
+            return get_item() != nullptr;
+        }
+
+        explicit operator bool()
+        {
+            return has_value_on_calling_thread();
+        }
+
+        void quiesce()
+        {
+            m_maps.quiesce();
+        }
+
+    private:
+
+        data_ptr get_item() const
+        {
+            auto& m = hive();
+            auto iter = m.lower_bound(this);
+            if (iter != m.end() && iter->first == this)
+                return &iter->second;
+
+            return nullptr;
+        }
+
+        data_ptr get_or_add_item () const
+        {
+            auto& m = hive();
+            auto iter = m.lower_bound(this);
+            if(iter != m.end() && iter->first == this)
+                return &iter->second;
+
+            m_maps.insert(&m);
+            iter = m.insert(iter, std::make_pair(this, m_initializer()));
+            GEOMETRIX_ASSERT(iter != m.end());
+            return &iter->second;
+        }
+
+        void destroy()
+        {
+            auto& m = hive();
+            auto key = this;
+            if (!m_deinitializer)
             {
-                m_deinitializer(it->second);
-                m.erase(it);
+                m.erase(key);
+                std::for_each(m_maps.begin(), m_maps.end(), [key, this](instance_map* pMap)
+                {
+                    pMap->erase(key);
+                });
             }
-            std::for_each(m_maps.begin(), m_maps.end(), [key, this](instance_map* pMap)
+            else
             {
-                auto it = pMap->find(key);
-                if (it != pMap->end())
+                auto it = m.find(key);
+                if (it != m.end())
                 {
                     m_deinitializer(it->second);
-                    pMap->erase(it);
+                    m.erase(it);
                 }
-            });
+                std::for_each(m_maps.begin(), m_maps.end(), [key, this](instance_map* pMap)
+                {
+                    auto it = pMap->find(key);
+                    if (it != pMap->end())
+                    {
+                        m_deinitializer(it->second);
+                        pMap->erase(it);
+                    }
+                });
+            }
+
+            GEOMETRIX_ASSERT(m.find(this) == m.end());
         }
 
-        GEOMETRIX_ASSERT(m.find(this) == m.end());
-    }
+        std::function<T()> m_initializer;
+        std::function<void(T&)> m_deinitializer;
+        mutable concurrent_set<instance_map*> m_maps;
 
-    std::function<T()> m_initializer;
-    std::function<void(T&)> m_deinitializer;
-    mutable concurrent_set<instance_map*> m_maps;
-
-};
+    };
 
 }}//! stk::thread.
 
