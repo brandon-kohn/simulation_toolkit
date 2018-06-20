196b13cafa34cb2cc4a1ba52ecf09406793699d8
Brandon Kohn <blkohn@hotmail.com>
2018-06-20 14:19:30 -0400

    Squashed commit of the following:
    commit 20a2dbbf107c3da8f48cc236f5075bce03602504
    Author: Brandon Kohn <blkohn@hotmail.com>
    Date:   Wed Jun 20 14:16:58 2018 -0400
    
        Updated geometrix.
    
    commit f2d0e3d258cf912a434ba14dad30ecdc0ef050b0
    Author: Brandon Kohn <blkohn@hotmail.com>
    Date:   Wed Jun 20 09:44:45 2018 -0400
    
        Updated geometrix.
    
    commit c874b546928f9f350513874cb977d6d118669259
    Author: Brandon Kohn <blkohn@hotmail.com>
    Date:   Wed Jun 20 09:19:56 2018 -0400
    
        Updated geometrix.
    
    commit 9d2609858666e292a7904265eae3fec84f1c4dc6
    Author: Brandon Kohn <blkohn@hotmail.com>
    Date:   Wed Jun 20 08:34:07 2018 -0400
    
        Updated geometrix.
    
    commit 5a1084aecbe3bed8ac53d45683484584c7c1d710
    Author: Brandon Kohn <blkohn@hotmail.com>
    Date:   Wed Jun 20 08:30:50 2018 -0400
    
        Updated geometrix.
    
    commit 1b427f6c59e73453bde305bc7354910de54ef206
    Author: Brandon Kohn <blkohn@hotmail.com>
    Date:   Wed Jun 20 08:25:16 2018 -0400
    
        Updated geometrix.
    
    commit af2f7cf133e16f8d1bad387fdffff1cfb7fca037
    Merge: dd8ac7f 4622410
    Author: Brandon Kohn <blkohn@hotmail.com>
    Date:   Wed Jun 20 08:11:40 2018 -0400
    
        Merge branch 'feature/fixed_point' of https://github.com/brandon-kohn/simulation_toolkit into feature/fixed_point
    
    commit 46224106aece96c739c550949742806a8c77fb6d
    Author: Brandon Kohn <blkohn@hotmail.com>
    Date:   Wed Jun 20 08:11:25 2018 -0400
    
        Updated geometrix and tests for fixed_point.
    
    commit dd8ac7f496ae1583ee1f1f9b724afb12897a8559
    Author: Brandon Kohn <blkohn@hotmail.com>
    Date:   Tue Jun 19 22:55:12 2018 -0400
    
        Set submodule geometrix.
    
    commit fe03a23a8f074d568d25c62fef30249af9c13c55
    Author: Brandon Kohn <blkohn@hotmail.com>
    Date:   Tue Jun 19 22:43:33 2018 -0400
    
        Added fixed_point_tests.cpp.

---
+++ test/fixed_point_tests.cpp
@@ -0,0 +1,66 @@
+#include <geometrix/numeric/fixed_point.hpp>
+#include <gtest/gtest.h>
+#include <cstdint>
+
+using fixed_point_3_traits = geometrix::fixed_point_traits
+<
+	geometrix::decimal_format_traits<std::int64_t>
+  , geometrix::generic_compile_time_scale_policy<10, 3>
+  , geometrix::round_half_to_even
+>;
+
+using fixed_point3 = geometrix::fixed_point<fixed_point_3_traits>;
+
+TEST(fixed_point_suite, construct_from_double)
+{
+	fixed_point3 sut(123.4567);
+
+	EXPECT_EQ(123.457, sut);
+}
+
+TEST(fixed_point_suite, addition)
+{
+	fixed_point3 sut(123.4567);
+
+	EXPECT_EQ(133.457, sut + 10.0);
+	EXPECT_EQ(133.457, 10. + sut);
+}
+
+TEST(fixed_point_suite, subtraction)
+{
+	fixed_point3 sut(123.4567);
+
+	EXPECT_EQ(113.457, sut - 10.0);
+	EXPECT_EQ(-113.457, 10. - sut);
+}
+
+
+TEST(fixed_point_suite, multiplication)
+{
+	fixed_point3 sut(123.4567);
+
+	EXPECT_EQ(1234.57, sut * 10.0);
+	EXPECT_EQ(1234.57, 10. * sut);
+}
+
+TEST(fixed_point_suite, division)
+{
+	fixed_point3 sut(123.4567);
+
+	EXPECT_EQ(12.345, sut / 10.0);
+}
+
+TEST(fixed_point_suite, division1_3)
+{
+	fixed_point3 sut(1.0);
+
+	EXPECT_EQ(0.333, sut / 3.0);
+}
+
+TEST(fixed_point_suite, division_fixed_1_3)
+{
+	fixed_point3 sut1(1.0);
+	fixed_point3 sut3(3.0);
+
+	EXPECT_EQ(0.333, sut1 / sut3);
+}
