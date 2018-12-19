edbadce9e9a948edfff5e7df06729b88d539db68
Brandon Kohn <blkohn@hotmail.com>
2018-12-19 14:24:45 -0500

    Updated flood fill. Added basic stk grid type.

---
+++ stk/stk/geometry/space_partition/grid.hpp
@@ -0,0 +1,72 @@
+//
+//! Copyright © 2018
+//! Brandon Kohn
+//
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+#pragma once
+
+#include <stk/units/boost_units.hpp>
+
+#include <geometrix/algorithm/hash_grid_2d.hpp>
+#include <geometrix/algorithm/fast_voxel_grid_traversal.hpp>
+
+namespace stk {
+
+    template <typename Cell>
+    class grid2
+    {
+        using grid_type = geometrix::hash_grid_2d<Cell, geometrix::grid_traits<stk::units::length>>;
+
+    public:
+
+        grid2(const geometrix::grid_traits<stk::units::length>& g)
+            : m_grid(g)
+        {}
+
+        template <typename Visitor>
+        void for_each_cell(const segment2& s, Visitor&& v)
+        {
+            auto fn = [&v, this](std::uint32_t i, std::uint32_t j)
+            {
+                auto& cell = m_grid.get_cell(i,j);
+                v(cell);
+            };
+	        geometrix::fast_voxel_grid_traversal(m_grid.get_traits(), s, fn, make_tolerance_policy());
+        }
+        
+        template <typename Visitor>
+        void for_each_cell(const polygon2& p, Visitor&& v)
+        {
+            auto fn = [&v, this](std::uint32_t i, std::uint32_t j)
+            {
+                auto& cell = m_grid.get_cell(i,j);
+                v(cell);
+            };
+            for(std::size_t i = 0, j = 1; i < s.size(); ++i, ++j %= size)  
+	            geometrix::fast_voxel_grid_traversal(m_grid.get_traits(), segment2{p[i], p[j]}, fn, make_tolerance_policy());
+        }
+        
+        template <typename Visitor>
+        void for_each_cell(const polyline2& p, Visitor&& v)
+        {
+            auto fn = [&v, this](std::uint32_t i, std::uint32_t j)
+            {
+                auto& cell = m_grid.get_cell(i,j);
+                v(cell);
+            };
+            for(std::size_t i = 0, j = 1; j < s.size(); i = j++)  
+	            geometrix::fast_voxel_grid_traversal(m_grid.get_traits(), segment2{p[i], p[j]}, fn, make_tolerance_policy());
+        }
+
+    private:
+
+        geometrix::hash_grid_2d<Cell, geometrix::grid_traits<stk::units::length>> m_grid;
+
+    };
+
+}//! namespace stk;
+
+#endif//STK_MESH_HPP
