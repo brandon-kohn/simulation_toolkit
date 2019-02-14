926ae7f598709a565b4d2054708bc13e7536b0df
Brandon Kohn <blkohn@hotmail.com>
2019-02-14 11:33:05 -0500

    Made constexpr if available.

--- stk/stk/utility/offset.hpp
+++ stk/stk/utility/offset.hpp
@@ -1,5 +1,5 @@
 //
-//! Copyright © 2014
+//! Copyright © 2019
 //! Brandon Kohn
 //
 //  Distributed under the Boost Software License, Version 1.0. (See
@@ -10,22 +10,41 @@
 
 #include <cmath>
 #include <type_traits>
+#include <boost/config.hpp>
 
 namespace stk{
-        template <typename T, typename Member, typename std::enable_if<std::is_standard_layout<T>::value, int>::type = 0>
-        inline std::size_t offset_of(Member T::* mbr)
-        {        
-            const T* p = nullptr;
-            return reinterpret_cast<const char*>(&(p->*mbr)) - reinterpret_cast<const char*>(p);
-        }
+    
+    template <typename T, typename Member>
+    BOOST_CONSTEXPR inline std::size_t offset_of(Member T::* mbr)
+    {
+        static_assert(std::is_standard_layout<T>::value, "T must be standard layout for offset_of to produce correct results. You may use offset_of_unsafe if you are certain the results will be correct.");
+        return (const char*)&(((const T*)nullptr)->*mbr) - (const char*)nullptr;
+    }
 
-        template <typename T, typename Member, typename std::enable_if<std::is_standard_layout<T>::value, int>::type = 0>
-        inline std::size_t offset_of(const T* p, Member T::* mbr)
-        {        
-            return std::abs(reinterpret_cast<const char*>(&(p->*mbr)) - reinterpret_cast<const char*>(p));
-        }
+    template <typename T, typename Member>
+    BOOST_CONSTEXPR inline std::size_t offset_of(const T* p, Member T::* mbr)
+    {
+        static_assert(std::is_standard_layout<T>::value, "T must be standard layout for offset_of to produce correct results. You may use offset_of_unsafe if you are certain the results will be correct.");
+        return std::abs((const char*)(&(p->*mbr)) - (const char*)p);
+    }
 
-#define STK_OFFSET_OF(T, Member) offset_of(&T::Member) 
+    #define STK_OFFSET_OF(T, Member) ::stk::offset_of(&T::Member)
+
+    //! Calculate unsafe offsets. These are considered unsafe because they don't enforce the standard-layout.
+    //! In cases of virtual inheritance or other complex inheritance the results are not known to be correct.
+    template <typename T, typename Member>
+    BOOST_CONSTEXPR inline std::size_t offset_of_unsafe(Member T::* mbr)
+    {
+        return (const char*)&(((const T*)nullptr)->*mbr) - (const char*)nullptr;
+    }
+
+    template <typename T, typename Member>
+    BOOST_CONSTEXPR inline std::size_t offset_of_unsafe(const T* p, Member T::* mbr)
+    {
+        return std::abs((const char*)(&(p->*mbr)) - (const char*)p);
+    }
+
+    #define STK_OFFSET_OF_UNSAFE(T, Member) ::stk::offset_of_unsafe((const T*)nullptr, &T::Member)
 
 }//! namespace stk;
 
