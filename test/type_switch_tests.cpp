927cd97bd710e6849c515b2d7b22aa0a9c7a6b90
Brandon Kohn <blkohn@hotmail.com>
2019-02-15 18:16:41 -0500

    Cleaned up test.

--- test/type_switch_tests.cpp
+++ test/type_switch_tests.cpp
@@ -26,64 +26,6 @@ struct d3_type : base2_type {};
 struct d4_type : base2_type {};
 TEST(memoization_device_suite, test)
 {
-	using T = base_type;
-	auto p1 = [](T* x) { return dynamic_cast<d1_type*>(x); };
-	auto p2 = [](T* x) { return dynamic_cast<d2_type*>(x); };
-	auto p3 = [](T* x) { return dynamic_cast<d3_type*>(x); };
-
-	auto s1 = [](T* x) { std::cout << "s1 was run.\n"; };
-	auto s2 = [](T* x) { std::cout << "s2 was run.\n"; };
-	auto s3 = [](T* x) { std::cout << "s3 was run.\n"; };
-	auto vtbl = [](const T* x)
-	{
-		static_assert(std::is_polymorphic<T>::value, "T must be a polymorphic type.");
-		return *reinterpret_cast<const std::intptr_t*>(x);
-	};
-
-	auto dswitch = [&](T* x)
-	{
-		using raw_type = std::decay<T>::type;
-		struct type_switch_info
-		{
-			type_switch_info(std::uint64_t d)
-				: data(d)
-			{}
-			
-			type_switch_info(std::int32_t o, std::uint32_t c)
-				: offset(o)
-				, case_n(c)
-			{}
-
-			union
-			{
-				std::int32_t offset;
-				std::uint32_t case_n;
-				std::uint64_t data;
-			};
-		};
-		static stk::concurrent_numeric_unordered_map<std::intptr_t, std::uint64_t> jump_targets;
-		auto key = vtbl(x);
-		const void* tptr;
-		type_switch_info info(jump_targets.insert(key, std::uint64_t{}).first);
-		switch (info.case_n) 
-		{
-			default:
-				if (tptr = p1(x)) { if(info.case_n == 0) jump_targets.assign(key, type_switch_info(std::intptr_t(tptr) - std::intptr_t(x), 1).data); case 1: s1(x); } else
-				if (tptr = p2(x)) { if(info.case_n == 0) jump_targets.assign(key, type_switch_info(std::intptr_t(tptr) - std::intptr_t(x), 2).data); case 2: s2(x); } else
-				if (tptr = p3(x)) { if(info.case_n == 0) jump_targets.assign(key, type_switch_info(std::intptr_t(tptr) - std::intptr_t(x), 3).data); case 3: s3(x); } else
-					case 4: break;//! no true predicates.
-		};
-	};
-
-	auto t1 = d1_type{};
-	auto t2 = d2_type{};
-	auto t3 = d3_type{};
-
-	dswitch(&t1);
-	dswitch(&t2);
-	dswitch(&t3);
-	dswitch(&t2);
-
 	using namespace stk;
 	auto sw = make_switch(
 	    make_case<d1_type>([&](d1_type* p)
@@ -99,6 +41,10 @@ TEST(memoization_device_suite, test)
 	        std::cout << "Type was d3_type.\n";
 	    })
 	);
+
+	auto t1 = d1_type{};
+	auto t2 = d2_type{};
+	auto t3 = d3_type{};
 
 	sw(&t1);
 	sw(&t2);
