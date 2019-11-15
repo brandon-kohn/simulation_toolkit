fc9d7a94abfb800c2b19c876b252a8e9f8848b03
Brandon Kohn <blkohn@hotmail.com>
2019-11-15 11:03:25 -0500

    Updated geometrix, junction and the macro engine for disable warning.

--- stk/stk/compiler/disable_warning.hpp
+++ stk/stk/compiler/disable_warning.hpp
@@ -8,21 +8,24 @@
 //
 
 #include <boost/config.hpp>
+#include <boost/preprocessor/facilities/is_empty.hpp>
 
 #ifndef STK_WARNING
 #error STK_WARNING not defined. This should be defined before including disable_warning.hpp.
 #endif
 
-#if defined(BOOST_MSVC)
-    #pragma warning(disable: STK_WARNING)
-#elif defined(BOOST_COMP_GNUC) && !defined(BOOST_COMP_CLANG)
-    #pragma GCC diagnostic ignored STK_WARNING
-#elif defined(BOOST_COMP_CLANG)
-    #if __has_warning(STK_WARNING)
-        #pragma clang diagnostic ignored STK_WARNING
-    #endif
-#else
+#if(!BOOST_PP_IS_EMPTY(STK_WARNING))
+    #if defined(BOOST_MSVC)
+        #pragma warning(disable: STK_WARNING)
+    #elif defined(BOOST_COMP_GNUC) && !defined(BOOST_COMP_CLANG)
+        #pragma GCC diagnostic ignored STK_WARNING
+    #elif defined(BOOST_COMP_CLANG)
+        #if __has_warning(STK_WARNING)
+            #pragma clang diagnostic ignored STK_WARNING
+        #endif
+    #else
 
-#endif//! disable STK_WARNING
+    #endif//! disable STK_WARNING
+#endif//! If there is a warning.
 
 #undef STK_WARNING
