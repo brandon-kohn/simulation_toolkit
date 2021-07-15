022acae136fa90dceca43538e98ff2195974df27
Brandon Kohn <blkohn@hotmail.com>
2021-07-15 10:08:27 -0400

    Added new 1.6.1 version.

---
+++ stk/stk/version.hpp
@@ -0,0 +1,14 @@
+//!  (C) Copyright Brandon Kohn 2008-2021.
+//!  Distributed under the Boost
+//!  Software License, Version 1.0. (See accompanying file
+//!  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+#ifndef STK_VERSION_HPP
+#define STK_VERSION_HPP
+#pragma once
+
+//!  STK_VERSION % 100 is the patch level
+//!  STK_VERSION / 100 % 1000 is the minor version
+//!  STK_VERSION / 100000 is the major version
+#define STK_VERSION 106100
+
+#endif
