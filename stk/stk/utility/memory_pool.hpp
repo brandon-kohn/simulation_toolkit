ccba38815db735fb461c278115ccdf55a06702a2
Brandon Kohn <blkohn@hotmail.com>
2022-03-30 18:40:22 -0400

    Fixed multiple scope issues with base.

--- stk/stk/utility/memory_pool.hpp
+++ stk/stk/utility/memory_pool.hpp
@@ -27,15 +27,15 @@ namespace stk {
 	struct constant_growth_policy
 	{
 		std::size_t initial_size() const { return Factor; }
-		std::size_t growth_factor(std::size_t) const { return Factor; }
+		std::size_t growth_factor( std::size_t ) const { return Factor; }
 	};
 	template <std::size_t InitialFactor>
 	struct geometric_growth_policy
 	{
 		std::size_t initial_size() const { return InitialFactor; }
-		std::size_t growth_factor(std::size_t capacity) const { return 2*capacity; }
+		std::size_t growth_factor( std::size_t capacity ) const { return 2 * capacity; }
 	};
-	
+
 	template <typename T>
 	class memory_pool_base
 	{
@@ -44,89 +44,87 @@ namespace stk {
 		struct node
 		{
 			node()
-                : storage{}
-                , pool{}
-            {}
-			
-            std::aligned_storage_t<sizeof( value_type ), alignof( value_type )> storage;
-            memory_pool_base<T>* pool;
+				: storage{}
+				, pool{}
+			{}
+
+			std::aligned_storage_t<sizeof( value_type ), alignof( value_type )> storage;
+			memory_pool_base<T>*                                                pool;
 		};
 		using block = std::vector<node>;
 
 	public:
-
-        static memory_pool_base* get_pool(value_type* v)
-        {
-            auto* n = reinterpret_cast<node*>(v);
+		static memory_pool_base* get_pool( value_type* v )
+		{
+			auto* n = reinterpret_cast<node*>( v );
 			return n->pool;
-        }
+		}
 
-		memory_pool_base(std::size_t initialSize = 8)
-			: m_blocks{ stk::make_vector<block>( block(initialSize) ) }
+		memory_pool_base( std::size_t initialSize = 8 )
+			: m_blocks{ stk::make_vector<block>( block( initialSize ) ) }
 		{
 			auto& blk = m_blocks.front();
 			auto  it = blk.begin();
-			auto gen = [&](){ return &*it++; };
+			auto  gen = [&]()
+			{ return &*it++; };
 			m_q.generate_bulk( gen, blk.size() );
 		}
-		
-		template <typename ... Args>
-		static value_type* construct(void* mem, Args&&...a)
+
+		template <typename... Args>
+		static value_type* construct( void* mem, Args&&... a )
 		{
 			GEOMETRIX_ASSERT( mem != nullptr );
 			::new( mem ) value_type( std::forward<Args>( a )... );
-            return reinterpret_cast<value_type*>(mem);
+			return reinterpret_cast<value_type*>( mem );
 		}
-		
-		static void destroy(value_type* v)
+
+		static void destroy( value_type* v )
 		{
-			if constexpr (std::is_destructible<value_type>::value)
+			if constexpr( std::is_destructible<value_type>::value )
 			{
 				v->~value_type();
 			}
 		}
 
-		void deallocate(value_type* v)
+		void deallocate( value_type* v )
 		{
-			enqueue( reinterpret_cast<node*>(v) );
+			enqueue( reinterpret_cast<node*>( v ) );
 		}
 
 		std::size_t size_free() const { return m_q.size_approx(); }
-		std::size_t size_elements() const 
+		std::size_t size_elements() const
 		{
 			auto size = std::size_t{};
-			for (const auto& blk : m_blocks) {
+			for( const auto& blk : m_blocks )
+			{
 				size += blk.size();
 			}
 			return size;
 		}
 
 	protected:
-
-		void enqueue(node* n)
+		void enqueue( node* n )
 		{
-            n->pool = nullptr;
+			n->pool = nullptr;
 			m_q.enqueue( n );
 			m_itemsLoaded.notify_one();
 		}
 
-		std::vector<block>                  m_blocks;
-		moodycamel::ConcurrentQueue<node*>  m_q;
-		std::mutex                          m_loadMutex;
-		std::condition_variable             m_itemsLoaded;
-
+		std::vector<block>                 m_blocks;
+		moodycamel::ConcurrentQueue<node*> m_q;
+		std::mutex                         m_loadMutex;
+		std::condition_variable            m_itemsLoaded;
 	};
 
 	template <typename T, typename GrowthPolicy = geometric_growth_policy<100>>
 	class memory_pool : public memory_pool_base<T>
 	{
 		using base_t = memory_pool_base<T>;
-		using node = base_t::node;
+		using node = typename base_t::node;
 		using value_type = T;
 		using growth_policy = GrowthPolicy;
 
 	public:
-		
 		memory_pool()
 			: base_t{ growth_policy().initial_size() }
 		{
@@ -139,51 +137,49 @@ namespace stk {
 		}
 
 	private:
-
 		value_type* dequeue()
 		{
 			node* n = nullptr;
-			while(!m_q.try_dequeue(n))
+			while( !base_t::m_q.try_dequeue( n ) )
 				expand();
-			if(n)
-            {
-                n->pool = this;
-				return std::launder(reinterpret_cast<value_type*>(&n->storage));
-            }
+			if( n )
+			{
+				n->pool = this;
+				return std::launder( reinterpret_cast<value_type*>( &n->storage ) );
+			}
 			return nullptr;
 		}
 
 		void expand()
 		{
 			bool isExpanding = false;
-			if (m_isExpanding.compare_exchange_strong(isExpanding, true))
+			if( m_isExpanding.compare_exchange_strong( isExpanding, true ) )
 			{
-				if( m_q.size_approx() == 0 )
+				if( base_t::m_q.size_approx() == 0 )
 				{
-					auto growSize = growth_policy().growth_factor( m_blocks.back().size() );
-					m_blocks.emplace_back( growSize );
-					auto it = m_blocks.back().begin();
+					auto growSize = growth_policy().growth_factor( base_t::m_blocks.back().size() );
+					base_t::m_blocks.emplace_back( growSize );
+					auto it = base_t::m_blocks.back().begin();
 					auto gen = [&]()
 					{ return &*it++; };
-					m_q.generate_bulk( gen, growSize );
+					base_t::m_q.generate_bulk( gen, growSize );
 				}
 				m_isExpanding.store( false );
-				m_itemsLoaded.notify_all();
+				base_t::m_itemsLoaded.notify_all();
 			}
-			else if( m_q.size_approx() == 0 )
+			else if( base_t::m_q.size_approx() == 0 )
 			{
-				auto lk = std::unique_lock<std::mutex>{ m_loadMutex };
-				m_itemsLoaded.wait( lk, [this](){ return !m_isExpanding.load( std::memory_order_relaxed ) || m_q.size_approx() > 0; } );
+				auto lk = std::unique_lock<std::mutex>{ base_t::m_loadMutex };
+				base_t::m_itemsLoaded.wait( lk, [this]()
+					{ return !m_isExpanding.load( std::memory_order_relaxed ) || base_t::m_q.size_approx() > 0; } );
 			}
-
 		}
 
-		std::atomic<bool>                   m_isExpanding;
-
+		std::atomic<bool> m_isExpanding;
 	};
 
 	template <typename U>
-	inline void deallocate_to_pool(U* v)
+	inline void deallocate_to_pool( U* v )
 	{
 		auto* pool = memory_pool_base<typename std::decay<U>::type>::get_pool( v );
 		GEOMETRIX_ASSERT( pool != nullptr );
