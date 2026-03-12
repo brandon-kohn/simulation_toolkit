d1e19cbb927c62f0c88617e407a0832639c61331
Brandon Kohn <blkohn@hotmail.com>
2026-03-12 13:19:26 +0000

    Updates and fixes to alignment and size limits of fixed_function.

--- stk/stk/thread/fixed_function.hpp
+++ stk/stk/thread/fixed_function.hpp
@@ -23,65 +23,69 @@ SOFTWARE.
 */
 #pragma once
 
-#include <type_traits>
-#include <cstring>
-#include <stdexcept>
-#include <utility>
 #include <boost/config.hpp>
+
+#include <cstddef>
+#include <new>
+#include <stdexcept>
+#include <type_traits>
+#include <utility>
 
 namespace stk
 {
-    /**
-     * @brief The fixed_function<R(ARGS...), STORAGE_SIZE> class implements
-     * functional object.
-     * This function is analog of 'std::function' with limited capabilities:
-     *  - It supports only move semantics.
-     *  - The size of functional objects is limited to storage size.
-     * Due to limitations above it is much faster on creation and copying than
-     * std::function.
-     */
-    template <typename SIGNATURE, size_t STORAGE_SIZE = 128>
+    //! The fixed_function<R(ARGS...), STORAGE_SIZE> class implements
+    //! a move-only function wrapper with fixed internal storage.
+    //!
+    //! Compared to std::function:
+    //! - only move semantics are supported
+    //! - callable size is limited to STORAGE_SIZE
+    //! - no dynamic allocation is performed
+    template <typename SIGNATURE, std::size_t STORAGE_SIZE = 128>
     class fixed_function;
 
-    template <typename R, typename... ARGS, size_t STORAGE_SIZE>
+    template <typename R, typename... ARGS, std::size_t STORAGE_SIZE>
     class fixed_function<R(ARGS...), STORAGE_SIZE>
     {
-        typedef R (*func_ptr_type)(ARGS...);
+        using func_ptr_type = R (*)(ARGS...);
+        using storage_type = std::aligned_storage_t<STORAGE_SIZE, alignof(std::max_align_t)>;
+        using method_type = R (*)(void* object_ptr, func_ptr_type free_func_ptr, ARGS&&... args);
+        using alloc_type = void (*)(void* storage_ptr, void* object_ptr);
 
     public:
+        static constexpr std::size_t storage_size = STORAGE_SIZE;
 
-        static const std::size_t storage_size = STORAGE_SIZE;
-
-        fixed_function()
+        fixed_function() noexcept
             : m_function_ptr(nullptr)
             , m_method_ptr(nullptr)
             , m_alloc_ptr(nullptr)
         {}
 
-        /**
-         * @brief fixed_function Constructor from functional object.
-         * @param object Functor object will be stored in the internal storage
-         * using move constructor. Unmovable objects are prohibited explicitly.
-         */
-        template <typename FUNC>
+        //! Construct from a functor/lambda stored in internal storage.
+        template <
+            typename FUNC,
+            typename unref_type = std::remove_reference_t<FUNC>,
+            typename = std::enable_if_t<!std::is_same_v<unref_type, fixed_function>>
+        >
         fixed_function(FUNC&& object)
             : fixed_function()
         {
-            typedef typename std::remove_reference<FUNC>::type unref_type;
+            static_assert(sizeof(unref_type) <= STORAGE_SIZE,
+                "functional object doesn't fit into internal storage");
+            static_assert(alignof(unref_type) <= alignof(storage_type),
+                "functional object requires stricter alignment than internal storage provides");
+            static_assert(std::is_move_constructible_v<unref_type>,
+                "functional object must be move constructible");
 
-            static_assert(sizeof(unref_type) < STORAGE_SIZE, "functional object doesn't fit into internal storage");
-            static_assert(std::is_move_constructible<unref_type>::value, "Should be of movable type");
-
-            m_method_ptr = [](void* object_ptr, func_ptr_type, ARGS... args) -> R
+            m_method_ptr = [](void* object_ptr, func_ptr_type, ARGS&&... args) -> R
             {
-                return static_cast<unref_type*>(object_ptr)->operator()(args...);
+                return (*static_cast<unref_type*>(object_ptr))(std::forward<ARGS>(args)...);
             };
 
             m_alloc_ptr = [](void* storage_ptr, void* object_ptr)
             {
                 if(object_ptr)
                 {
-                    unref_type* x_object = static_cast<unref_type*>(object_ptr);
+                    auto* x_object = static_cast<unref_type*>(object_ptr);
                     new(storage_ptr) unref_type(std::move(*x_object));
                 }
                 else
@@ -90,101 +94,108 @@ namespace stk
                 }
             };
 
-            m_alloc_ptr(&m_storage, &object);
+            new(&m_storage) unref_type(std::forward<FUNC>(object));
         }
 
-        /**
-         * @brief fixed_function Constructor from free function or static member.
-         */
+        //! Construct from a free function or static member function.
         template <typename RET, typename... PARAMS>
-        fixed_function(RET (*func_ptr)(PARAMS...))
+        fixed_function(RET (*func_ptr)(PARAMS...)) noexcept
             : fixed_function()
         {
-            m_function_ptr = func_ptr;
-            m_method_ptr = [](void*, func_ptr_type f_ptr, ARGS... args) -> R
+            m_function_ptr = reinterpret_cast<func_ptr_type>(func_ptr);
+
+            m_method_ptr = [](void*, func_ptr_type f_ptr, ARGS&&... args) -> R
             {
-                return static_cast<RET (*)(PARAMS...)>(f_ptr)(args...);
+                auto typed_ptr = reinterpret_cast<RET (*)(PARAMS...)>(f_ptr);
+                return typed_ptr(std::forward<ARGS>(args)...);
             };
         }
 
-        fixed_function(fixed_function&& o)
+        fixed_function(fixed_function&& other) noexcept
             : fixed_function()
         {
-            moveFromOther(o);
+            moveFromOther(other);
         }
 
-        fixed_function& operator=(fixed_function&& o)
+        fixed_function& operator=(fixed_function&& other) noexcept
         {
-            moveFromOther(o);
+            if(this != &other)
+            {
+                reset();
+                moveFromOther(other);
+            }
             return *this;
         }
 
+        fixed_function(const fixed_function&) = delete;
+        fixed_function& operator=(const fixed_function&) = delete;
+
         ~fixed_function()
         {
-            if(m_alloc_ptr)
-                m_alloc_ptr(&m_storage, nullptr);
+            reset();
         }
 
-        /**
-         * @brief operator () Execute stored functional object.
-         * @throws std::runtime_error if no functional object is stored.
-         */
+        //! Execute the stored callable.
+        //! Throws std::runtime_error if no callable is stored.
         BOOST_FORCEINLINE R operator()(ARGS... args) const
         {
-            if(!m_method_ptr) 
+            if(!m_method_ptr)
                 throw std::runtime_error("call of empty functor");
-            return m_method_ptr((void*)&m_storage, m_function_ptr, args...);
+
+            return m_method_ptr(const_cast<void*>(static_cast<const void*>(&m_storage)),
+                                m_function_ptr,
+                                std::forward<ARGS>(args)...);
         }
 
-        bool empty() const 
+        BOOST_FORCEINLINE bool empty() const noexcept
         {
-            return !m_method_ptr;
+            return m_method_ptr == nullptr;
+        }
+
+        BOOST_FORCEINLINE explicit operator bool() const noexcept
+        {
+            return !empty();
+        }
+
+        void reset() noexcept
+        {
+            if(m_alloc_ptr)
+            {
+                m_alloc_ptr(&m_storage, nullptr);
+            }
+
+            m_function_ptr = nullptr;
+            m_method_ptr = nullptr;
+            m_alloc_ptr = nullptr;
         }
 
     private:
-
-        fixed_function& operator=(const fixed_function&) = delete;
-        fixed_function(const fixed_function&) = delete;
-
         union
         {
-            typename std::aligned_storage<STORAGE_SIZE, sizeof(size_t)>::type m_storage;
+            storage_type m_storage;
             func_ptr_type m_function_ptr;
         };
 
-        typedef R (*method_type)(void* object_ptr, func_ptr_type free_func_ptr, ARGS... args);
         method_type m_method_ptr;
-
-        typedef void (*alloc_type)(void* storage_ptr, void* object_ptr);
         alloc_type m_alloc_ptr;
 
-        void moveFromOther(fixed_function& o)
+        void moveFromOther(fixed_function& other) noexcept
         {
-            if(this == &o) 
-                return;
+            m_method_ptr = other.m_method_ptr;
+            m_alloc_ptr = other.m_alloc_ptr;
 
-            if(m_alloc_ptr)
+            if(other.m_alloc_ptr)
             {
-                m_alloc_ptr(&m_storage, nullptr);
-                m_alloc_ptr = nullptr;
+                m_alloc_ptr(&m_storage, &other.m_storage);
             }
             else
             {
-                m_function_ptr = nullptr;
+                m_function_ptr = other.m_function_ptr;
             }
 
-            m_method_ptr = o.m_method_ptr;
-            o.m_method_ptr = nullptr;
-
-            if(o.m_alloc_ptr)
-            {
-                m_alloc_ptr = o.m_alloc_ptr;
-                m_alloc_ptr(&m_storage, &o.m_storage);
-            }
-            else
-            {
-                m_function_ptr = o.m_function_ptr;
-            }
+            other.m_method_ptr = nullptr;
+            other.m_alloc_ptr = nullptr;
+            other.m_function_ptr = nullptr;
         }
     };
-}//! namespace stk;
+} //! namespace stk
